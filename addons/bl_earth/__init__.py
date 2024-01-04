b59d4eab3a7c523e0a5a051b6172c194661ad57b
StephanSiemen <stephan.siemen@gmail.com>
2024-01-04 22:00:39 +0000

    add basic file selector

--- addons/bl_earth/__init__.py
+++ addons/bl_earth/__init__.py
@@ -24,10 +24,12 @@ from bl_earth import operators
 def register():
     bpy.utils.register_class(operators.OBJECT_OT_creator_earth)
     bpy.utils.register_class(panels.BlEarth_UI_PT_panel)
+    bpy.utils.register_class(panels.OBJECT_OT_file_path)
 
 def unregister():
     bpy.utils.unregister_class(operators.OBJECT_OT_creator_earth)
     bpy.utils.unregister_class(panels.BlEarth_UI_PT_panel)
+    bpy.utils.unregister_class(panels.OBJECT_OT_file_path)
 
 #
 #  blender --background --python __init__.py -noaudio -E 'CYCLES' -f 1 -F 'PNG'
--- addons/bl_earth/panels.py
+++ addons/bl_earth/panels.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 import bpy
+from bpy.props import StringProperty
+
 from bl_earth import operators
+
+
+
+class OBJECT_OT_file_path(bpy.types.Operator):
+    """Move Objects up or down by given offset"""
+    bl_idname = "object.file_path"
+    bl_label = "Select file"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    data_file : StringProperty(
+        name="Data File",
+        description="Only ...",
+        subtype="FILE_PATH")
+    
+    def execute(self, context):
+        return {'FINISHED'}
 
 class BlEarth_UI_PT_panel(bpy.types.Panel):
     bl_idname = "BLEARTH_PT_3Dview"
@@ -15,3 +33,4 @@ class BlEarth_UI_PT_panel(bpy.types.Panel):
         col.operator(operators.OBJECT_OT_creator_earth.bl_idname, text="Create globe")
         col.separator()
         col.label(text="Blender overlays")
+        col.operator(OBJECT_OT_file_path.bl_idname, text="Select file ...")
