13b080760fab714346aaa4bb936ade84d95bd421
StephanSiemen <stephan.siemen@gmail.com>
2024-01-20 11:47:55 +0000

    Doc batch startup file

--- bl_earth.py
+++ bl_earth.py
@@ -1,5 +1,20 @@
+#
+#   Script to run bl_earth in batch mode
+#
+#    Example:
+#      blender --background --python bl_earth.py -noaudio -E 'CYCLES' -f 1 -F 'PNG'
+#
+
+#
+#  First set path to find bl_earth in the addon folder 
+#
 import sys, os
+
 sys.path.append(os.path.join(os.path.dirname(__file__),'addons'))
+
+#
+#  Now we can import module
+#
 import bl_earth
 
 bl_earth.run()
