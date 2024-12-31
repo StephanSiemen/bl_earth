32d8c6502475d51df75b142842fb8bc7c253ea84
StephanSiemen <stephan.siemen@gmail.com>
2024-12-31 12:03:46 +0000

    render generated PNGs

--- addons/bl_earth/data.py
+++ addons/bl_earth/data.py
@@ -10,7 +10,7 @@ def read_data(filename):
     print("NumPy:   ", np.__version__)
     print("xarray:  ", xr.__version__)
 
-    animation = dict()
+    fc_data = dict()
 
     print("------------------- "+str(filename)+" ----------------")
 
@@ -18,7 +18,7 @@ def read_data(filename):
     # ds = xr.open_mfdataset(filename, combine='nested', concat_dim='time',
     #                  engine='cfgrib',backend_kwargs={'indexpath': ''})
 
-    animation['ds'] = ds
+    #fc_data['ds'] = ds
 
     #a list with the variable names of your xarray.Dataset
     names = list(ds.data_vars)
@@ -33,31 +33,37 @@ def read_data(filename):
         print(f"Attributes of variable '{var}':", ds[var].attrs)
 
     for var_name, da in ds.data_vars.items():
+       steps = dict()
        if 'step' in da.dims:
             for time in da.step:
-                print(f">>>>>>>>>>>>>>>>>>>>>>>>>>> {var_name}")
+                # Extract hour values for steps (in nanoseconds)
                 fc_step = str((time.values / 3600000000000.).astype(int)).zfill(3)
-                # Select the data for the current time
                 data_slice = da.sel(step=time)
-            
+
                 # Create the plot
                 fig = plt.figure(frameon=False, figsize=(10, 6))
                 plt.axis('off')
                 plt.title('')
                 data_slice.plot(cmap=plt.cm.coolwarm, add_colorbar=False)
-            
+
                 # Save the plot
                 filename = f"/tmp/bl_earth_{var_name}_{fc_step}.png"
                 plt.savefig(filename, dpi=300, bbox_inches='tight',transparent=True, pad_inches=0)
-                plt.close()
-            
-    #  animation[str(i)] = [outfile,var]
+                
+                steps[fc_step] = filename
+
+            fc_data[var_name] = steps
 
         #  values = ds[var][i,:,:].values
         #  plt.imshow(values, interpolation='none')
   
-    animation["options"] = options
-    return animation
+    #plt.close()
+    fc_data["options"] = options
+    print("*********************************")
+    print(fc_data)
+    del ds
+    print("*********************************")
+    return fc_data
 
 #
 #  python data.py file.grb
--- addons/bl_earth/operators.py
+++ addons/bl_earth/operators.py
@@ -8,6 +8,7 @@ class OBJECT_OT_creator_earth(bpy.types.Operator):
     """Create collections based on objects types"""
     bl_idname = "object.bl_earth_creator"
     bl_label = "Create globe"
+    bl_options = {'REGISTER', 'UNDO'}
 
     clear_scene: bpy.props.BoolProperty(
                         name="Clear current scene?",
@@ -35,13 +36,18 @@ class OBJECT_OT_creator_earth(bpy.types.Operator):
 
 #####################################################
 
-bl_options = []
+variables = []
+
+# def get_collections(self, context):
+#     global variables
+#     print("*********>>>>>>>>>>>>>>>>>>>>>" + str(variables))
+#     return variables
 
 class OBJECT_OT_create_layer(bpy.types.Operator):
     """Create data overlays on the globe"""
     bl_idname = "object.bl_earth_layer"
     bl_label = "Create layer"
-
+    bl_options = {'REGISTER', 'UNDO'}
     
     clear_scene: bpy.props.BoolProperty(
                         name="Clear current layers?",
@@ -53,8 +59,11 @@ class OBJECT_OT_create_layer(bpy.types.Operator):
     variable: bpy.props.EnumProperty(
                         name="Variable",
                         description="Choose a variable",
-                        items=lambda self, context: bl_options
-    )
+                        items=lambda self, context: variables['options']
+                        # items={
+                        #     ('OPT_tp', 'tp - Total precipitation in m', 'Total precipitation in m'),
+                        #     ('OPT_t2m', 't2m - 2 metre temperature in K', '2 metre temperature in K')}
+                            )
 
     @classmethod
     def poll(cls, context):
@@ -66,7 +75,7 @@ class OBJECT_OT_create_layer(bpy.types.Operator):
 
     def execute(self, context):
         self.report({'INFO'}, f"bl_earth - Variable selected: {self.variable}")
-        render.render_layers(self.clear_scene, self.radius)
+        render.render_layers(self.clear_scene, self.radius, variables)
         return {'FINISHED'}
 
 
@@ -75,10 +84,19 @@ class OBJECT_OT_create_layer(bpy.types.Operator):
 class OBJECT_OT_file_path(bpy.types.Operator, ImportHelper):
     bl_idname = "blearth.invoke_file_chooser"
     bl_label = "Select file"
-    bl_options = {'REGISTER', 'UNDO'}
+    #bl_options = {'REGISTER', 'UNDO'}
 
+    filter_glob:  bpy.props.StringProperty(
+        default='*.grib;*.grb;*.grib2;*.nc',
+        options={'HIDDEN'} )
+
+    @classmethod
+    def poll(cls, context):
+        return True
+    
     def execute(self, context):
-        global bl_options 
-        input = data.read_data(self.filepath)
-        bl_options = input["options"]
+        global variables 
+        variables = data.read_data(self.filepath)
+        print(">>>>>>>>>>>" + self.filepath)
+        print("*********************^^^^^^^^^^^^^^^^^^^^*22222" + str(variables))
         return {'FINISHED'}
